e33a7154d2ee3f077ddddd31554a2bde42aa081e
Crystofher Lins <crystofher_lins@outlook.com>
2021-07-21 16:17:30 -0300

    storage picture adjustments

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,18 +1,30 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
 
+import Picture from '../models/Picture';
+
 const upload = multer(multerConfig).single('picture');
 
 class PictureController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+  store(req, res) {
+    return upload(req, res, async (error) => {
       if (error) {
         return res.status(400).json({
           errors: [error.code],
         });
       }
 
-      return res.json(req.file);
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const picture = await Picture.create({ originalname, filename, aluno_id });
+
+        return res.json(picture);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
     });
   }
 }
---
+++ src/database/migrations/20210721145158-criar-tabela-de-foto-do-aluno-corrigida.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable('pictures', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      aluno_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'alunos',
+          key: 'id',
+        },
+        // Apaga a foto qnd o aluno for apagado
+        // onDelete: 'CASCADE',
+
+        // Aluno_id recebe valor nulo qnd o aluno for apagado
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    queryInterface.dropTable('pictures');
+  },
+};
